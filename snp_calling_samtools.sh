540e2628c8597e91f7020c34edbe864cae35bb1a
LakshmanTejaG <lakshmanteja@gmail.com>
2018-09-30 18:10:49 +0530

    Removed Files

--- snp_calling_samtools.sh
+++
@@ -1,5 +0,0 @@
-#! /usr/bin/bash -v
-samtools index sorted_Strain_ST398.bam 
-samtools mpileup -ugf scaffolds.fasta sorted_Strain_ST398.bam | bcftools call -cv -> var.raw.vcf
-bcftools view var.raw.vcf | vcfutils.pl varFilter -D100 > ST398_final.vcf 
-
