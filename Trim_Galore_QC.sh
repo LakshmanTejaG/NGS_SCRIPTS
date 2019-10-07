151dc0e4b67f75a2b2e7d11920a2ec3839a9ae0d
LAKSHMANTEJA <lakshmanteja@gmail.com>
2019-10-07 13:47:00 +0530

    Add files via upload

---
+++ Trim_Galore_QC.sh
@@ -0,0 +1,2 @@
+trim_galore --phred33 --quality 30 --illumina --paired --fastqc  ERR162499_1.fastq ERR162499_2.fastq
+trim_galore --phred33 --quality 30 -a ACACTCTTTCCCTACACGACGCTCTTCCGATCT -a2 CTCGGCATTCCTGCTGAACCGCTCTTCCGATCT --paired --fastqc  ERR162499_1.fastq ERR162499_2.fastq
